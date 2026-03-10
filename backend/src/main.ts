b8de0c81eb6546b9bc1208fda063be42b478b57c
Aasrith22 <aasrith1424nimmagadda@gmail.com>
2026-03-10 14:19:07 +0530

    feat : containerization

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // frontend URL
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3001',
     credentials: true,
   });
 
