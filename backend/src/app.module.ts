a48d6f71e15670a5a7e03459382981ca24431730
sihabutheen8117 <sihabutheen8117@gmail.com>
2026-03-10 05:49:33 +0530

    feat:implemented guards , enums , decorators

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule ,AuthModule ],
   providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
 })
 export class AppModule {}
